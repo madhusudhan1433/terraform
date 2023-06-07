3b4d0b8bcd8941937c5f201f03e03c5d1af52d61
madhusudhan1433 <madhusudhandany@gmail.com>
2023-06-07 20:59:07 +0530

    updated terraform

--- terra.tf
+++ terra.tf
@@ -1,14 +1,14 @@
 provider  "aws" {
-region = "ap-south-1"
-access_key = "AKIA2W5ZZXZOTA5MUHDE"
-secret_key = "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"
+region = "us-east-1"
+access_key = "AKIA2W5ZZXZOVNTLI4W3"
+secret_key = "GG25JQNLig3d36fFMsR+n3MEDbKDsd8Q/vfUNthb"
 }
 
 resource "aws_instance" "example" {
 ami     = "ami-053b0d53c279acc90"
 instance_type = "t2.micro"
-key_name      = "jenkins"
+key_name      = "jenkinsmaster"
 tags          = {
-Name          = "vcube"
+Name          = "madhu"
 }
 }
